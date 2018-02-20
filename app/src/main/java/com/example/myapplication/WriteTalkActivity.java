24c782e0cd8f60fcd6243bee761a545bce3d21ff
manjoong <skekf123@naver.com>
2018-02-20 14:58:54 +0900

    successretrofit

--- app/src/main/java/com/example/myapplication/WriteTalkActivity.java
+++ app/src/main/java/com/example/myapplication/WriteTalkActivity.java
@@ -1,12 +1,16 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import retrofit2.Call;
@@ -22,34 +26,49 @@ import static com.example.myapplication.APIservice.API_URL;
  */
 
 public class WriteTalkActivity extends AppCompatActivity {
-    TextView textViewIndex;
     private EditText input_id;
     private EditText input_pwd;
     private EditText input_title;
     private EditText input_content;
-    Button input_button;
+    TextView tv_title;
+    ImageButton btn_write;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_communuty);
-        textViewIndex = (TextView) findViewById(R.id.txtRetrofitTest);
+        setContentView(R.layout.activity_write_talk);
         input_id = findViewById(R.id.input_id);
         input_pwd = findViewById(R.id.input_pwd);
         input_title = findViewById(R.id.input_title);
         input_content = findViewById(R.id.input_content);
-        input_button = findViewById(R.id.input_button);
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        btn_write = (ImageButton) findViewById(R.id.btn_write);
 
 
-
-        input_button.setOnClickListener(new View.OnClickListener() {
+        //공유버튼 클릭시! alert창
+        btn_write.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                post();
+            public void onClick(View view) {
+                AlertDialog.Builder alert_confirm = new AlertDialog.Builder(WriteTalkActivity.this);
+                alert_confirm.setMessage("글을 게시하겠습니까?").setCancelable(false).setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                post();
+                            }
+                        }).setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                return;
+                            }
+                        });
+                AlertDialog alert = alert_confirm.create();
+                alert.show();
 
             }
         });
 
+        initialize_toolbar();
         index();
     }
 
@@ -103,6 +122,18 @@ public class WriteTalkActivity extends AppCompatActivity {
         });
     }
 
+
+    // 툴바 제어
+    void initialize_toolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // Attaching the layout to the toolbar object
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.icon_back);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        tv_title.setText("자유게시판");
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -115,6 +146,6 @@ public class WriteTalkActivity extends AppCompatActivity {
     }
 
 
-
-
 }
+
+
